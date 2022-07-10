12f18bb319d9a941309ac2e8aa0c04076927d4ff
Shahaf Dan <dan.shachaf@gmail.com>
2022-07-10 19:20:44 -0400

    jupyter notebook switch work

--- src/blocks/sections.js
+++ src/blocks/sections.js
@@ -5,51 +5,44 @@ import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 
 function Sections() {
-    const [apiname, setCart] = useState();
-    const [, updateState] = React.useState();
-    const forceUpdate = React.useCallback(() => updateState({}), []);
-    const updateApiName = (name) => {
-        setCart([apiname, name]);
-
-    }
-
+    const [api, setApi] = useState('nba')
     return(
         <div className='sections-container'>
             <div className='sections'>
-                <button className='section' onClick={() => updateApiName('nba')}> <a href="/nba"> NBA </a></button>
-                <button className='section' onClick={() => updateApiName('spotify')}> <a href="/spotify"> Spotify </a></button>
-                <button className='section' onClick={() => updateApiName('yahoofinance')}> <a href="/yahoofin"> Yahoo Finance </a></button>
+                <button className='section' onClick={() => setApi(api => api = 'nba')}> 
+                     NBA 
+                </button>
+                <button className='section' onClick={() => setApi(api => api = 'spotify')}> 
+                    Spotify 
+                </button>
+                <button className='section' onClick={() => setApi(api => api = 'yahoofinance')}> 
+                    Yahoo Finance 
+                </button>
             </div>
-            <Notebook value={apiname} />
+            <div className='notebook-container'>
+                <JupyterNotebookViewer
+                    filePath={"apis/"+(api)+"/analyze.ipynb"} // Or a raw JSON notebook file location online
+                    // filePath={"apis/nba/analyze.ipynb"} // Or a raw JSON notebook file location online
+                    notebookInputLanguage="python"
+                    className="notebook-class"
+                    inputCodeDarkTheme={true}
+                    outputDarkTheme={true}
+                    inputMarkdownDarkTheme={true}
+                    showInputLineNumbers={true}
+                    showOutputLineNumbers={false}
+                    withOnClick={true}
+                    hideCodeBlocks={false}
+                    hideMarkdownBlocks={false}
+                    hideAllOutputs={false}
+                    hideAllInputs={false}
+                    remarkPlugins={[remarkMath]}
+                    rehypePlugins={[rehypeKatex]}
+                />
+            </div>
         </div>
     )
     
+    
 }
-
-function Notebook(props) {
-    return (
-        <div className='notebook-container'>
-            <JupyterNotebookViewer
-                filePath={"apis/"+(props.value)+"/analyze.ipynb"} // Or a raw JSON notebook file location online
-                // filePath={"apis/nba/analyze.ipynb"} // Or a raw JSON notebook file location online
-                notebookInputLanguage="python"
-                className="notebook-class"
-                inputCodeDarkTheme={true}
-                outputDarkTheme={true}
-                inputMarkdownDarkTheme={true}
-                showInputLineNumbers={true}
-                showOutputLineNumbers={false}
-                withOnClick={true}
-                hideCodeBlocks={false}
-                hideMarkdownBlocks={false}
-                hideAllOutputs={false}
-                hideAllInputs={false}
-                remarkPlugins={[remarkMath]}
-                rehypePlugins={[rehypeKatex]}
-            />
-        </div>  
-    )
-}
-
 
 export default Sections;
